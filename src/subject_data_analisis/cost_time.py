6b6d3179d94bf983b2299f432e7698e13a9ccaed
lucianopaz <chulo1787@gmail.com>
2015-09-16 14:00:22 -0300

    Fixed bugs in cost_time.py

--- src/subject_data_analisis/cost_time.py
+++ src/subject_data_analisis/cost_time.py
@@ -28,8 +28,10 @@ class Model():
 		self.n = int(n)
 		if self.n%2==0:
 			self.n+1
-		self.g = np.linspace(0,1,self.n)
-		self.t = np.arange(0,T+dt,dt,np.float)
+		self.g = np.linspace(0.,1.,self.n)
+		self.dt = float(dt)
+		self.T = float(T)
+		self.t = np.arange(0.,float(T+dt),float(dt),np.float)
 		self.nT = self.t.shape[0]
 		self.p = self.belief_transition_p()
 		self.cost = cost*np.ones_like(self.t)
@@ -60,24 +62,25 @@ class Model():
 		return optimize.brentq(f,a,b)
 	
 	def belief_transition_p(self):
-		xx = np.zeros((self.nT,self.n))
+		invg = np.zeros((self.nT,self.n))
 		post_var = np.zeros(self.nT)
 		p = np.zeros((self.n,self.nT-1,self.n))
 		
 		for i,t in enumerate(self.t):
 			for j,g in enumerate(self.g):
 				if j>0:
-					xx[i,j] = self.g2x(t,g,xx[i,j-1])
+					invg[i,j] = self.g2x(t,g,invg[i,j-1])
 				else:
-					xx[i,j] = self.g2x(t,g)
+					invg[i,j] = self.g2x(t,g)
 			post_var[i] = self.post_mu_var(t)
 		for i,t in enumerate(self.t[:-1]):
 			for j,g in enumerate(self.g):
-				mu_n = self.post_mu_mean(t,xx[i,j])
+				mu_n = self.post_mu_mean(t,invg[i,j])
 				for k,g_1 in enumerate(self.g):
-					p[k,i,j] = np.exp(-0.5*(xx[i+1,k]-xx[i,j]-mu_n)**2/(post_var[i]+self.model_var))/\
-							  np.exp(-0.5*(xx[i+1,k]/post_var[i+1]+self.prior_mu_mean/self.prior_mu_var)**2*post_var[i+1])
-		p = p/np.sum(p,axis=0)/(self.g[1]-self.g[0])
+					p[k,i,j] = -0.5*(invg[i+1,k]-invg[i,j]-mu_n)**2/(post_var[i]+self.model_var)+\
+							   0.5*(invg[i+1,k]/self.model_var+self.prior_mu_mean/self.prior_mu_var)**2*post_var[i+1]
+		# To avoid overflows, we substract the max value in the numerator and denominator's exponents
+		p = np.exp(p-np.max(p,axis=0))/np.sum(np.exp(p-np.max(p,axis=0)),axis=0)/(self.g[1]-self.g[0])
 		return np.transpose(p,(1,2,0))
 	
 	def value_dp(self,reward=1.,penalty=0.,iti=1.,tp=0.):
@@ -88,6 +91,7 @@ class Model():
 		
 		f = lambda x: self.backpropagate_value(x)
 		self.rho = optimize.brentq(f,-10,10)
+		self.decision_bounds()
 	
 	def backpropagate_value(self,rho=None):
 		if rho is not None:
@@ -96,7 +100,7 @@ class Model():
 		v2 = self.reward*(1-self.g)-self.penalty-(self.iti+self.g*self.tp)*self.rho
 		self.value = np.zeros((self.nT,self.n))
 		self.value[-1] = np.max(np.array([v1,v2]),axis=0)
-		dt = self.t[1]-self.t[0]
+		dt = self.dt
 		dg = self.g[1]-self.g[0]
 		for i,t in reversed(list(enumerate(self.t[:-1]))):
 			v_explore = np.sum(self.value[i+1]*self.p[i]*dg,axis=1)-(self.cost[i]+self.rho)*dt
@@ -105,11 +109,11 @@ class Model():
 		return self.value[0,int(0.5*self.n)]
 	
 	def decision_bounds(self):
+		self.bounds = np.zeros((self.nT,2))
 		v1 = self.reward*self.g-self.penalty*(1-self.g)-(self.iti+(1-self.g)*self.tp)*self.rho
 		v2 = self.reward*(1-self.g)-self.penalty-(self.iti+self.g*self.tp)*self.rho
-		bounds = np.zeros((self.nT,2))
-		decide_1 = v1==self.value
-		decide_2 = v2==self.value
+		decide_1 = np.abs((v1-self.value)/v1)<1e-9
+		decide_2 = np.abs((v2-self.value)/v1)<1e-9
 		for i,t in enumerate(self.t):
 			if any(decide_1[i]):
 				bound1 = self.g[decide_1[i].nonzero()[0][0]]
@@ -119,12 +123,34 @@ class Model():
 				bound2 = self.g[decide_2[i].nonzero()[0][-1]]
 			else:
 				bound2 = 0.
-			bounds[i] = np.array([bound1,bound2])
-		return bounds
+			self.bounds[i] = np.array([bound1,bound2])
+		return self.bounds
+	
+	def refine_value(self,dt,n):
+		change = False
+		if dt<self.dt:
+			oldt = self.t.copy()
+			self.t = np.arange(0,self.T+dt,dt,np.float)
+			self.nT = self.t.shape[0]
+			self.cost = np.interp(self.t, oldt, self.cost)
+			change = True
+		n = int(n)
+		if n%2==0:
+			n+=1
+		if n>self.n:
+			self.n = n
+			self.g = np.linspace(0.,1.,self.n)
+			change = True
+		if change:
+			self.p = self.belief_transition_p()
+			self.backpropagate_value(rho=self.rho)
+			self.decision_bounds()
 
 def test():
 	m = Model(model_var=10,prior_mu_var=40,n=101,T=10.,cost=1)
-	b = m.decision_bounds()
+	#~ print m.rho
+	#~ m.refine_value(dt=1e-2,n=501)
+	b = m.bounds
 	plt.figure(figsize=(13,10))
 	colors = plt.get_cmap('jet')
 	plt.subplot(121)
@@ -135,7 +161,7 @@ def test():
 	plt.subplot(122)
 	plt.plot(m.t,b)
 	plt.xlabel('T [s]')
-	plt.xlabel('Bound')
+	plt.ylabel('Bound')
 	plt.show()
 
 if __name__=="__main__":
