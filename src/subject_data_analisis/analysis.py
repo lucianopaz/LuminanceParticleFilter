Fixed bugs, added documentation and improved the jacobian computation for several scipy optimization calls.

src/subject_data_analisis/analysis.py
@@ -192,26 +192,25 @@ class Analyzer():
 		full_confidence_merit = fitter.forced_compute_full_confidence_merit(parameters)
 		full_merit = fitter.forced_compute_full_merit(parameters)
 		confidence_only_merit = fitter.forced_compute_confidence_only_merit(parameters)
-		model_prediction,t_array = fitter.theoretical_rt_confidence_distribution()
-		dt = fitter.dp.dt
+		fitter_stats = fitter.stats(return_mean_rt=True,return_mean_confidence=True,return_median_rt=True,
+				return_median_confidence=True,return_std_rt=True,return_std_confidence=True,
+				return_auc=True)
+		
 		c_array = np.linspace(0,1,fitter.confidence_partition)
 		norm = np.sum(model_prediction)
 		norm0 = np.sum(model_prediction[0])
 		norm1 = np.sum(model_prediction[1])
-		performance = norm0/norm
-		confidence = np.sum(np.sum(model_prediction,axis=0)*c_array[:,None])/norm
-		hit_confidence = np.sum(model_prediction[0]*c_array[:,None])/norm0
-		miss_confidence = np.sum(model_prediction[1]*c_array[:,None])/norm1
-		rt = np.sum(np.sum(model_prediction,axis=0)*t_array[None,:])/norm
-		hit_rt = np.sum(model_prediction[0]*t_array[None,:])/norm0
-		miss_rt = np.sum(model_prediction[1]*t_array[None,:])/norm1
+		performance = fitter_stats['performance']
+		performance_conditioned = fitter_stats['performance_conditioned']
+		confidence = fitter_stats['mean_confidence']
+		hit_confidence = fitter_stats['mean_confidence_conditioned'][0]
+		miss_confidence = fitter_stats['mean_confidence_conditioned'][1]
+		rt = fitter_stats['mean_rt']
+		rt_conditioned = np.sum(fitter_stats['mean_rt_conditioned']*performance_conditioned,axis=1)
+		hit_rt = rt_conditioned[0]
+		miss_rt = rt_conditioned[1]
 		
-		pconf_hit = np.hstack((np.zeros(1),np.cumsum(np.sum(model_prediction[0],axis=1)*dt)))
-		pconf_hit/=pconf_hit[-1]
-		pconf_miss = np.hstack((np.zeros(1),np.cumsum(np.sum(model_prediction[1],axis=1)*dt)))
-		pconf_miss/=pconf_miss[-1]
-		
-		auc = scipy.integrate.trapz(pconf_miss,pconf_hit)
+		auc = fitter_stats['auc']
 		
 		key = 'experiment_'+fitter.experiment+'_subject_'+fitter.subjectSession.get_name()+'_session_'+fitter.subjectSession.get_session()
 		out = {key:{'experiment':fitter.experiment,'parameters':parameters,'full_merit':full_merit,\
@@ -679,16 +678,16 @@ def parameter_correlation(method='full_confidence', optimizer='cma', suffix='', 
 				n_clusters=2, affinity='euclidean', linkage='ward', pooling_func=np.nanmean,\
 				merge='names',filter_nans='post', tree_mode='r',show=False,extension='svg'):
 	a = Analyzer(method, optimizer, suffix, override, n_clusters, affinity, linkage, pooling_func)
-	parameters,parameter_names,names,sessions,experiments = \
-		a.get_parameter_array_from_summary(normalize={'internal_var':'experiment'})
 	#~ parameters,parameter_names,names,sessions,experiments = \
-		#~ a.get_parameter_array_from_summary(normalize={'internal_var':'experiment',\
-													  #~ 'confidence_map_slope':'all',\
-													  #~ 'cost':'all',\
-													  #~ 'high_confidence_threshold':'all',\
-													  #~ 'dead_time':'all',\
-													  #~ 'dead_time_sigma':'all',\
-													  #~ 'phase_out_prob':'all'})
+		#~ a.get_parameter_array_from_summary(normalize={'internal_var':'experiment'})
+	parameters,parameter_names,names,sessions,experiments = \
+		a.get_parameter_array_from_summary(normalize={'internal_var':'experiment',\
+													  'confidence_map_slope':'all',\
+													  'cost':'all',\
+													  'high_confidence_threshold':'all',\
+													  'dead_time':'all',\
+													  'dead_time_sigma':'all',\
+													  'phase_out_prob':'all'})
 	
 	dtype = [('parameters','O'),('name','i'),('session','i'),('experiment',experiments.dtype)]
 	sort_array = [(p,int(n),int(s),e) for p,n,s,e in zip(parameters,names,sessions,experiments)]

src/subject_data_analisis/figures.py
@@ -826,7 +826,7 @@ def confidence_mapping(fname='confidence_mapping',suffix='.svg'):
 	xytext_cont = [fitter.dp.t[np.argmin(np.abs(cont_map[0]-0.2))+2],0.2]
 	
 	fpt = fitter.dp.rt(0,fitter.dp.xbounds())
-	bin_pdf = fitter.confidence_mapping_pdf_matrix(fpt,parameters,bin_map)
+	bin_pdf = fitter.rt_confidence_pdf(fpt,parameters,bin_map)
 	t = np.arange(0,bin_pdf.shape[-1],dtype=np.float)*fitter.dp.dt
 	
 	gs = gridspec.GridSpec(2, 1,left=0.54, right=0.94,hspace=0.25)
@@ -840,7 +840,7 @@ def confidence_mapping(fname='confidence_mapping',suffix='.svg'):
 	
 	vmin = 1e-5
 	
-	cont_pdf = fitter.confidence_mapping_pdf_matrix(fpt,parameters,cont_map)
+	cont_pdf = fitter.rt_confidence_pdf(fpt,parameters,cont_map)
 	ax3 = plt.subplot(gs[1])
 	if not vmin is None:
 		cont_pdf[cont_pdf<vmin] = vmin

src/subject_data_analisis/test.py
@@ -25,8 +25,16 @@ all_model_rt_low = {'2AFC':{'t':[],'pdf':[]},'Auditivo':{'t':[],'pdf':[]},'Lumin
 n = {'2AFC':0,'Auditivo':0,'Luminancia':0}
 edges = {'2AFC':np.linspace(0,6,51),'Auditivo':np.linspace(0,6,51),'Luminancia':np.linspace(0,1,51)}
 for s in subjects:
-	fitter_fname = fits.Fitter_filename(experiment=s.experiment,method='full_confidence',name=s.get_name(),
-				session=s.get_session(),optimizer='cma',suffix='')
+	#~ if s.get_name()!='12':
+		#~ continue
+	#~ fitter_fname = fits.Fitter_filename(experiment=s.experiment,method='full_confidence',name=s.get_name(),
+				#~ session=s.get_session(),optimizer='cma',suffix='')
+	#~ fitter_fname = fits.Fitter_filename(experiment=s.experiment,method='full_binary_confidence',name=s.get_name(),
+				#~ session=s.get_session(),optimizer='Nelder-Mead',suffix='')
+	fitter_fname = fits.Fitter_filename(experiment=s.experiment,method='full_binary_confidence',name=s.get_name(),
+				session=s.get_session(),optimizer='basinhopping',suffix='',confidence_map_method='belief')
+	#~ fitter_fname = fits.Fitter_filename(experiment=s.experiment,method='binary_confidence_only',name=s.get_name(),
+				#~ session=s.get_session(),optimizer='basinhopping',suffix='',confidence_map_method='belief')
 	f = open(fitter_fname,'r')
 	fitter = pickle.load(f)
 	f.close()
@@ -59,6 +67,11 @@ for s in subjects:
 	all_model_rt_high[s.experiment]['pdf'].append(binary_confidence_pdf[1]*float(len(perf)))
 	all_model_rt_low[s.experiment]['pdf'].append(binary_confidence_pdf[0]*float(len(perf)))
 	n[s.experiment]+= float(len(perf))
+	#~ plt.step(edges[s.experiment],np.hstack((rt_high/normalization,(rt_high/normalization)[-1])),'forestgreen',label='Subject high',where='post')
+	#~ plt.step(edges[s.experiment],np.hstack((rt_low/normalization,(rt_low/normalization)[-1])),'mediumpurple',label='Subject low',where='post')
+	#~ plt.plot(t,binary_confidence_pdf[1],'forestgreen',linewidth=3,label='Theoretical high')
+	#~ plt.plot(t,binary_confidence_pdf[0],'mediumpurple',linewidth=3,label='Theoretical low')
+	#~ plt.show(True)
 
 plt.figure()
 experiment_alias = {'2AFC':'Contrast','Auditivo':'Auditory','Luminancia':'Luminance'}
@@ -78,11 +91,11 @@ for i,k in enumerate(all_rt_high.keys()):
 		model_low[:len(l)]+=l
 	
 	rt_centers = np.array([0.5*(e1+e0) for e1,e0 in zip(edges[k][1:],edges[k][:-1])])
-	ax = plt.subplot(plot_gs[row_index[k]-1])
-	all_rt_high[k] = np.array(all_rt_high[k])
-	all_rt_low[k] = np.array(all_rt_low[k])
-	plt.step(rt_centers,np.nansum(all_rt_high[k]/n[k],axis=0),'forestgreen',label='Subject high')
-	plt.step(rt_centers,np.nansum(all_rt_low[k]/n[k],axis=0),'mediumpurple',label='Subject low')
+	ax = plt.subplot(plot_gs[row_index[k]-1,0])
+	all_rt_high[k] = np.nansum(np.array(all_rt_high[k])/n[k],axis=0)
+	all_rt_low[k] = np.nansum(np.array(all_rt_low[k])/n[k],axis=0)
+	plt.step(edges[k],np.hstack((all_rt_high[k],all_rt_high[k][-1])),'forestgreen',label='Subject high',where='post')
+	plt.step(edges[k],np.hstack((all_rt_low[k],all_rt_low[k][-1])),'mediumpurple',label='Subject low',where='post')
 	plt.plot(t,model_high/n[k],'forestgreen',linewidth=3,label='Theoretical high')
 	plt.plot(t,model_low/n[k],'mediumpurple',linewidth=3,label='Theoretical low')
 	ax.set_xlim(xlim[k])
@@ -90,6 +103,19 @@ for i,k in enumerate(all_rt_high.keys()):
 		plt.legend(loc='best', fancybox=True, framealpha=0.5)
 	elif row_index[k]==3:
 		plt.xlabel('RT [s]')
+	
+	#~ ax = plt.subplot(plot_gs[row_index[k]-1,1])
+	#~ high_ratio = all_rt_high[k]/(all_rt_high[k]+all_rt_low[k])
+	#~ high_ratio_std = np.sqrt(high_ratio*(1-high_ratio)/(all_rt_high[k]+all_rt_low[k])/n[k])
+	#~ high_ratio_std[high_ratio_std<1e-2] = 1e-2
+	#~ plt.plot(rt_centers,high_ratio,color='b',label='Subject high')
+	#~ plt.fill_between(rt_centers,high_ratio+high_ratio_std,high_ratio-high_ratio_std,color='b',alpha=0.4,label='Subject high ratio')
+	#~ plt.plot(t,model_high/(model_high+model_low),'r',linewidth=2,label='Theoretical high ratio')
+	#~ ax.set_xlim(xlim[k])
+	#~ if row_index[k]==1:
+		#~ plt.legend(loc='best', fancybox=True, framealpha=0.5)
+	#~ elif row_index[k]==3:
+		#~ plt.xlabel('RT [s]')
 	
 	aximg = plt.subplot(exp_scheme_gs[row_index[k]-1])
 	exp_scheme = mpimg.imread('../../figs/'+k+'.png')